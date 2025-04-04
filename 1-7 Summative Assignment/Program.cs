aa919bdcbc4a1e24aabd453b38fb1800dc9ec7b5
Hannah Anderson <339729345@GoTVDSB.ca>
2025-04-04 14:29:44 -0400

    Add project files.

---
+++ 1-7 Summative Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1_7_Summative_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
